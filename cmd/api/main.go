432899e1989e5b1594c68396babf7b0cdfd1f3bd
Valdeci Gomes <contact@valde.ci>
2022-03-31 08:52:27 -0300

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type AppStatus struct {
+	Status      string `json:"status"`
+	Environment string `json:"environment"`
+	Version     string `json:"version"`
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to list on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
+	flag.Parse()
+
+	fmt.Println("Running")
+
+	http.HandleFunc("/status", func(writer http.ResponseWriter, request *http.Request) {
+		currentStatus := AppStatus{
+			Status:      "Available",
+			Environment: cfg.env,
+			Version:     version,
+		}
+
+		j, err := json.MarshalIndent(currentStatus, "", "\t")
+		if err != nil {
+			log.Println(err)
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		_, err = writer.Write(j)
+		if err != nil {
+			return
+		}
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,1 @@
+module games-shelf-api-go
