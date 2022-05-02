16bf8a41dadba0201d23ba6b6835c896764b7343
Valdeci Gomes <9141708+coderade@users.noreply.github.com>
2022-05-01 21:00:55 -0300

    Add db close logicOC

--- internal/api/api.go
+++ internal/api/api.go
@@ -13,23 +13,27 @@ type Server struct {
 	Config *config.Config
 	Shelf  *models.Shelf
 	Logger *logger.Logger
+	DB     *sql.DB
 }
 
+// Initialize initializes the server with the provided configuration and logger.
 func (s *Server) Initialize(cfg *config.Config, log *logger.Logger) {
 	s.Config = cfg
 	s.Logger = log
 
 	db, err := openDBConnection(cfg, log)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to open database connection: ", err)
 	}
+	s.DB = db
 	s.Shelf = models.NewShelf(db)
 }
 
+// openDBConnection opens a database connection and pings it to ensure it's reachable.
 func openDBConnection(cfg *config.Config, log *logger.Logger) (*sql.DB, error) {
 	db, err := sql.Open("postgres", cfg.Db.Dsn)
 	if err != nil {
-		log.Error(err)
+		log.Error("Error opening database connection: ", err)
 		return nil, err
 	}
 
@@ -38,9 +42,22 @@ func openDBConnection(cfg *config.Config, log *logger.Logger) (*sql.DB, error) {
 
 	err = db.PingContext(ctx)
 	if err != nil {
-		log.Error(err)
+		log.Error("Error pinging database: ", err)
 		return nil, err
 	}
 
+	log.Info("Successfully connected to the database")
 	return db, nil
+}
+
+// Close closes the database connection.
+func (s *Server) Close() {
+	if s.DB != nil {
+		err := s.DB.Close()
+		if err != nil {
+			s.Logger.Error("Error closing database connection: ", err)
+		} else {
+			s.Logger.Info("Database connection closed")
+		}
+	}
 }
